12083a7bc6a5fd4fb3cc5285478a53f731bd1c68
Jordi-B19 <63915364+Jordi-B19@users.noreply.github.com>
2020-07-23 19:09:37 +0200

    React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
@@ -14,9 +15,11 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+    </BrowserRouter>
     );
   }
 
